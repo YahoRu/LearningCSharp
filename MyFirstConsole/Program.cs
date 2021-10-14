3fca7cd8e4df6f4d0c392558d9eb1064811ed50f
Yahor Ruban <rubanyahor@gmail.com>
2021-10-14 09:48:49 +0300

    Add project files.

---
+++ MyFirstConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberTen = 10;
+            Console.WriteLine(numberTen);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MySecondConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MySecondConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.141592653;
+            decimal e = 1.414213562373095048m;
+
+            Console.WriteLine(pi + "\n" + e);
+            Console.ReadKey();
+        }
+    }
+}
