aa735bfec561c6a52a57282deba13ff3b4beac48
Yahor Ruban <rubanyahor@gmail.com>
2021-10-21 09:57:47 +0300

    черновик занятия 3 часть 3

---
+++ Lesson3part3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Lesson3part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayLengh = int.Parse(Console.ReadLine());
+            int[] array = new int[arrayLengh];
+           
+
+            for (int i = 0; i < arrayLengh; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int biggestIntInArray = 0;
+
+            foreach (int var in array)
+            {
+                if (var > biggestIntInArray) { biggestIntInArray = var; }
+            }
+
+            int lowestIntInArray = biggestIntInArray;
+
+            foreach (int var in array)
+            {
+                if (var < lowestIntInArray) { lowestIntInArray = var; }
+            }
+
+            for (int i = biggestIntInArray; i >= lowestIntInArray; i--)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                link:
+                    if ((array[j] % i) == 0)
+                    {
+
+                        if (j == (arrayLengh - 1))
+                        {
+                            Console.WriteLine("НОД :" + i);
+                            Console.ReadKey();
+                            return;
+                        }
+                        else if ((array[j] % i) == 0 && (j != (arrayLengh - 1)))
+                        {
+                            ++j;
+                            goto link;
+                        }
+                    }
+                    else break;
+
+                }
+            }
+
+
+        }
+    }
+}
