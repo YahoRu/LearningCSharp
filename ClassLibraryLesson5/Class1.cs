53c3578eb046057226780c029113cb92e99134a0
Yahor Ruban <rubanyahor@gmail.com>
2021-10-28 18:03:11 +0300

    Lesson 5, part 1-2

---
+++ ClassLibraryLesson5/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassLibraryLesson5
+{
+    public class ArrayWorker
+    {
+
+        public int[] Invert(ref int[] array)
+        {
+            int[] newArray = new int[array.Length];
+
+            int count = array.Length - 1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[count];
+                count--;
+            }
+            return array = newArray;
+        }
+
+        public int[] MassiveChanger(ref int[] source, int index, int newValue)
+        {
+            int[] newSource = new int[source.Length + 1];
+
+            for (int i = 0; i < newSource.Length; i++)
+            {
+                if (i < index)
+                {
+                    newSource[i] = source[i];
+                }
+                else if (i == index)
+                {
+                    newSource[i] = newValue;
+                }
+                else
+                {
+                    newSource[i] = source[i - 1];
+                }
+            }
+            return source = newSource;
+        }
+    }
+   
+}
---
+++ Lesson5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ClassLibraryLesson5;
+
+namespace Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+            ArrayWorker worker1 = new ArrayWorker();
+
+            worker1.Invert(ref array);
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            worker1.MassiveChanger(ref array, 3, 5);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+
+        }
+    }
+}
