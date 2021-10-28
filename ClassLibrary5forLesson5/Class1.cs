e3239d97f884dcee1d175a31a071233c89488e68
Yahor Ruban <rubanyahor@gmail.com>
2021-10-28 18:00:32 +0300

    Задание 5, ч 3

---
+++ ClassLibrary5forLesson5/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassLibrary5forLesson5
+{
+    public class Cone
+    {
+        private double Radius { get; set; }
+        private double High { get; set; }
+
+        public Cone(double r, double h)
+        {
+            Radius = r;
+            High = h;
+        }
+
+        public double SurfaceArea()
+        {
+            double x = (Math.PI * Math.Pow(Radius, 2));
+            Console.WriteLine(x);
+            return (Math.PI * Math.Pow(Radius, 2));
+        }
+
+
+    }
+}
---
+++ Lesson5part3try/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary5forLesson5;
+
+namespace Lesson5part3try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cone cone = new Cone(1.0, 3.0);
+
+            double x = cone.SurfaceArea();
+
+            Console.WriteLine(x);
+        }
+    }
+}
