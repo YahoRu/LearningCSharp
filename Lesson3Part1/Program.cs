c5a9c38a24277e0b06c106f70781050b620d3732
Yahor Ruban <rubanyahor@gmail.com>
2022-03-10 12:49:45 +0300

    Lessson3Part3

--- Lesson3Part1/Program.cs
+++ Lesson3Part1/Program.cs
@@ -7,17 +7,15 @@ namespace Lesson3Part1
         static void Main(string[] args)
         {
             int a, b;
-            Console.WriteLine("Введите первое число: ");
+            Console.WriteLine("Введите два числа через Enter: ");
 
             int.TryParse(Console.ReadLine(), out a);
 
-            Console.WriteLine("Введите второе число: ");
-
             int.TryParse(Console.ReadLine(), out b);
 
-            if (a > b) Console.WriteLine(a+b);
-            else if (a < b) Console.WriteLine(a-b);
-            else Console.WriteLine(a*b);
+            if (a > b) Console.WriteLine(a + b);
+                else if (a < b) Console.WriteLine(a - b);
+                    else Console.WriteLine(a * b);
         }
     }
 }
---
+++ Lesson3Part3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lesson3Part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Введите три числа через Enter: ");
+
+            int.TryParse(Console.ReadLine(), out a);
+
+            int.TryParse(Console.ReadLine(), out b);
+
+            int.TryParse(Console.ReadLine(), out c);
+
+            if (a >= b && a >= c)
+            {
+                if (b >= c) Console.WriteLine($"{c}, {b}, {a}");
+                else Console.WriteLine($"{b}, {c}, {a}");
+            }
+            else if (b >= a && b >= c)
+            {
+                if (a >= c) Console.WriteLine($"{c}, {a}, {b}");
+                else Console.WriteLine($"{a}, {c}, {b}");
+            }
+            else
+            {
+                if (a >= b) Console.WriteLine($"{b}, {a}, {c}");
+                else Console.WriteLine($"{a}, {b}, {c}");
+            }
+
+        }
+    }
+}
