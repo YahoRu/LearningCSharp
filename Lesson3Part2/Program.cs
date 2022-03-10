25752275843ad17ccece83f13df1a7bc20c35cc8
Yahor Ruban <rubanyahor@gmail.com>
2022-03-10 11:41:01 +0300

    part2

---
+++ Lesson3Part2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson3Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("Введите два числа через Enter: ");
+
+            int.TryParse(Console.ReadLine(), out x);
+
+            int.TryParse(Console.ReadLine(), out y);
+
+            if (x > 0 && y > 0) Console.WriteLine($"Точка с координатами {x},{y} пренадлежит первой четверти");
+            else if(x> 0 && y < 0) Console.WriteLine($"Точка с координатами {x},{y} пренадлежит второй четверти");
+            else if(x < 0 && y < 0) Console.WriteLine($"Точка с координатами {x},{y} пренадлежит третьей четверти");
+            else if(x < 0 && y > 0) Console.WriteLine($"Точка с координатами {x},{y} пренадлежит четвертой четверти");
+            else Console.WriteLine($"Одна или несколько точек {x},{y} является нулем и лежит на пересечении линии координат");
+        }
+    }
+}
