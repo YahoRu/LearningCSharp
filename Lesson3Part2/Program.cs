faa2266fa908a6f8be0184ca3d1f5658bf1353ca
Yahor Ruban <rubanyahor@gmail.com>
2022-03-10 11:41:53 +0300

    upd part2

--- Lesson3Part2/Program.cs
+++ Lesson3Part2/Program.cs
@@ -14,9 +14,9 @@ namespace Lesson3Part2
             int.TryParse(Console.ReadLine(), out y);
 
             if (x > 0 && y > 0) Console.WriteLine($"Точка с координатами {x},{y} пренадлежит первой четверти");
-            else if(x> 0 && y < 0) Console.WriteLine($"Точка с координатами {x},{y} пренадлежит второй четверти");
-            else if(x < 0 && y < 0) Console.WriteLine($"Точка с координатами {x},{y} пренадлежит третьей четверти");
-            else if(x < 0 && y > 0) Console.WriteLine($"Точка с координатами {x},{y} пренадлежит четвертой четверти");
+            else if (x > 0 && y < 0) Console.WriteLine($"Точка с координатами {x},{y} пренадлежит второй четверти");
+            else if (x < 0 && y < 0) Console.WriteLine($"Точка с координатами {x},{y} пренадлежит третьей четверти");
+            else if (x < 0 && y > 0) Console.WriteLine($"Точка с координатами {x},{y} пренадлежит четвертой четверти");
             else Console.WriteLine($"Одна или несколько точек {x},{y} является нулем и лежит на пересечении линии координат");
         }
     }
