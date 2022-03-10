8f0cb2d8c1fb9ebfc3ccc188df87f15920fff42f
Yahor Ruban <rubanyahor@gmail.com>
2022-03-10 11:16:45 +0300

    part1

---
+++ Lesson3Part1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson3Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Введите первое число: ");
+
+            int.TryParse(Console.ReadLine(), out a);
+
+            Console.WriteLine("Введите второе число: ");
+
+            int.TryParse(Console.ReadLine(), out b);
+
+            if (a > b) Console.WriteLine(a+b);
+            else if (a < b) Console.WriteLine(a-b);
+            else Console.WriteLine(a*b);
+        }
+    }
+}
