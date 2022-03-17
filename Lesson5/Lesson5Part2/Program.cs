7e7ef7062b0e0c34add21eff3d99902c22e7ccf1
YahoRu <81859291+YahoRu@users.noreply.github.com>
2022-03-17 14:08:58 +0300

    part2

---
+++ Lesson5/Lesson5Part2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson5Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
